3842c3f22b1bdac6bb9a9252694eaf4a46c9ebb5
Richard Felix <rfelixmail@gmail.com>
2015-09-15 18:05:11 -0400

    working need email still

--- Justice Version/justice-law-business-html-template/pack/justice/process/mail.php
+++ Justice Version/justice-law-business-html-template/pack/justice/process/mail.php
@@ -1,155 +1,49 @@
 <?php
- 
-if(isset($_POST['email'])) {
- 
-     
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
-    $email_to = "rfelixmail@gmail.com";
- 
-    $email_subject = "Your email subject line";
- 
-     
- 
-     
- 
-    function died($error) {
- 
-        // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
- 
-     
- 
-    // validation expected data exists
- 
-    if(!isset($_POST['first_name']) ||
- 
-        !isset($_POST['last_name']) ||
- 
-        !isset($_POST['email']) ||
- 
-        !isset($_POST['telephone']) ||
- 
-        !isset($_POST['comments'])) {
- 
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
- 
-    }
- 
-     
- 
-    $first_name = $_POST['first_name']; // required
- 
-    $last_name = $_POST['last_name']; // required
- 
-    $email_from = $_POST['email']; // required
- 
-    $telephone = $_POST['telephone']; // not required
- 
-    $comments = $_POST['comments']; // required
- 
-     
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
- 
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
- 
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($comments) < 2) {
- 
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
-     
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-     
- 
-     
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
- 
-?>
- 
- 
- 
-<!-- include your own success html here -->
- 
- 
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
- 
- 
-<?php
- 
+// Information to be modified
+$to_email = "rfelixmail@gmail.com"; // email address to which the form data will be sent
+$subject = "Contact Request"; // subject of the email that is sent
+if(trim($_POST["contact_name"])=='')
+{
+echo 'Please Fill The Name Field';
 }
- 
-?>
+elseif(trim($_POST["contact_email"])=='')
+{
+echo 'Please Fill The Valid Email Field';
+}elseif (filter_var($_POST["contact_email"], FILTER_VALIDATE_EMAIL) === false) {
+  echo $_POST["contact_email"]." is not a valid email address";
+} 
+elseif(trim($_POST["contact_message"])=='')
+{
+echo 'Please Fill The Message';
+}
+else
+{
+	$con='';
+$nam = strip_tags($_POST["contact_name"]);
+$ema = strip_tags($_POST["contact_email"]);
+if(isset($_POST["contact_phoneno"]) && $_POST["contact_phoneno"]!='')
+$con = strip_tags($_POST["contact_phoneno"]);
+$com = strip_tags($_POST["contact_message"]);
+
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: <' .$ema. '>' . "\r\n";
+$headers .= "Reply-To: ".$ema."\r\n";
+
+$email_body = 
+	"<strong>From: </strong>" . $nam . "<br />
+	<strong>Email: </strong>" . $ema . "<br />";
+	if(isset($_POST["contact_phoneno"]) && $_POST["contact_phoneno"]!='')
+$email_body .="<strong>Contact No: </strong>" . $con . "<br />";	
+	$email_body .="<strong>Message: </strong>" . $com;
+	
+
+// Assuming there's no error, send the email and redirect to Thank You page
+	
+if( mail($to_email, $subject, $email_body, $headers) ) {	
+	echo '<i class="glyphicon glyphicon-ok"></i> Thank you ' .$nam. '. Your Email was successfully sent!';
+} else {	
+	echo '<i class="glyphicon glyphicon-remove"></i> Sorry ' .$nam. '. Your Email was not sent. Resubmit form again Please..';
+}
+}
