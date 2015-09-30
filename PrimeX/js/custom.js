2de5ce436701ee1cd785eb7146ebc67a2b9052de
Richard Felix <rfelixmail@gmail.com>
2015-09-30 03:10:31 -0400

    cleaned up customjs mobile function for bgs and fixed up background css for main pic

--- PrimeX/js/custom.js
+++ PrimeX/js/custom.js
@@ -28,22 +28,13 @@ $(document).ready(function() {
      Background Image
 
     --------------------------------------------------- */
-        bgImage();
+     bgImage();
 
     function bgImage() {
-        var pageSection = $(".image-bg");
-        pageSection.each(function(indx) {
-            if ($(this).attr("data-background")) {
-                 if(jQuery.browser.mobile){  
-                     $('#bg').attr('data-background', 'img/bg/bgMobile2.jpg');
-                     $('.hero-content').attr('class','hero-content left-middle-block1 z-index9');
-                 }else{
-                     $('#bg').attr('data-background', 'img/bg/bg1.jpg');
-                 }    
-    
-                $(this).css("background-image", "url(" + $(this).data("background") + ")");
-            }
-        });
+        if(jQuery.browser.mobile){  
+            $('.image-bg').css("background-image", 'url(../img/bg/bgMobile2.jpg)');  
+            $('.hero-content').attr('class','hero-content left-middle-block1 z-index9');
+        }  
     }
     
     /*----------------------------------
