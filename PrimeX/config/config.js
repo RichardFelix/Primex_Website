1af97eca220cbfd15215f90bd4125647a2774f05
Richard Felix <me@richardcfelix.com>
2016-01-07 15:52:08 -0500

    updated flightplan and added config file

---
+++ PrimeX/config/config.js
@@ -0,0 +1,7 @@
+module.exports = config = {
+    sendgridUser: 'rfelix',
+    sendgridPwd: 'richiesd22',
+    mongoLocation_dev: 'mongodb://localhost/primex',
+    mongoLocation_production: 'mongodb://rfelixMongo:richie21mb@ds035965.mongolab.com:35965/primex',
+    emailLocation: 'rfelixmail@gmail.com'
+};
