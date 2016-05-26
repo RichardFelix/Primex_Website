a525172792049b76a0c30aab3f4c0f1a6df63af0
Richard Felix <rfelixmail@gmail.com>
2016-05-25 21:56:52 -0400

    deleted node_modules flightplan and config files

--- PrimeX/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = config = {
-    sendgridUser: 'rfelix',
-    sendgridPwd: 'richiesd22',
-    mongoLocation_dev: 'mongodb://localhost/primex',
-    mongoLocation_production: 'mongodb://rfelixMongo:richie21mb@ds035965.mongolab.com:35965/primex',
-    emailLocation: 'rfelixmail@gmail.com'
-};
